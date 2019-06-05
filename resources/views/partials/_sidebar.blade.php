2a8b8db4ea1eb5a284e801c77e27eae2ce763ca6
Mukes32 <mukeshrai3449@gmail.com>
2019-06-05 18:04:59 +0545

    msg

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,26 +1,41 @@
 <div class="navbar-default sidebar" role="navigation">
         <div class="sidebar-nav slimscrollsidebar">
             <div class="sidebar-head">
-                <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span></h3> </div>
+                <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">TaskTracker</span></h3> </div>
             <ul class="nav" id="side-menu">
-                <li><a href="javascript:void(0)" class="waves-effect active"><i data-icon="7" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Link type </span></a> </li>
-                <li> <a href="javascript:void(0)" class="waves-effect"><i data-icon="/" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Dropdown Link<span class="fa arrow"></span><span class="label label-rouded label-purple pull-right">2</span></span></a>
+                <li><a href="javascript:void(0)" class="waves-effect active"><i data-icon="7" class="linea-icon linea-basic fa-fw"></i><span                class="hide-menu">Dashboard</span></a> 
+                </li>
+                <li> <a href="javascript:void(0)" class="waves-effect"><i data-icon="/" class="glyphicon glyphicon-user"></i><span class="hide-menu">Staff Management<span class="fa arrow"></span><span class="label label-rouded label-purple pull-right">2</span></span></a>
                     <ul class="nav nav-second-level">
-                        <li><a href="javascript:void(0)"><i data-icon=")" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Second Level Item</span></a></li>
-                        <li><a href="javascript:void(0)"><i class="fa-fw">S</i><span class="hide-menu"> Second Level Item</span></a></li>
+                        <li><a href="javascript:void(0)"><i data-icon=")" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Register staff</span></a></li>
+                        <li><a href="javascript:void(0)"><i class="fa-fw">S</i><span class="hide-menu">view Staff</span></a></li>
                     </ul>
                 </li>
-                <li> <a href="javascript:void(0)" class="waves-effect"><i data-icon="" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Multi Dropdown<span class="fa arrow"></span></span></a>
+                <li> <a href="javascript:void(0)" class="waves-effect"><i data-icon="" class="glyphicon glyphicon-tasks"></i><span class="hide-menu">Task Management<span class="fa arrow"></span></span></a>
                     <ul class="nav nav-second-level">
-                        <li> <a href="javascript:void(0)"><i data-icon="/" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Second Level Item</span></a> </li>
-                        <li> <a href="javascript:void(0)"><i data-icon="7" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Second Level Item</span></a> </li>
-                        <li> <a href="javascript:void(0)" class="waves-effect"><i data-icon="&#xe008;" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Third Level </span><span class="fa arrow"></span></a>
+                        <li> <a href="javascript:void(0)"><i data-icon="/" class="glyphicon glyphicon-pencil"></i><span class="hide-menu">Create new task</span></a> </li>
+                        <li> <a href="javascript:void(0)"><i data-icon="7" class="glyphicon glyphicon-share"></i><span class="hide-menu">Assign Task</span></a> </li>
+                        <li> <a href="javascript:void(0)" class="waves-effect"><i data-icon="&#xe008;" class="glyphicon glyphicon-eye-open"></i><span class="hide-menu">View all task</span><span class="fa arrow"></span></a>
                             <ul class="nav nav-third-level">
-                                <li> <a href="javascript:void(0)"><i class=" fa-fw">T</i><span class="hide-menu">Third Level Item</span></a> </li>
-                                <li> <a href="javascript:void(0)"><i class=" fa-fw">M</i><span class="hide-menu">Third Level Item</span></a> </li>
-                                <li> <a href="javascript:void(0)"><i class=" fa-fw">R</i><span class="hide-menu">Third Level Item</span></a> </li>
-                                <li> <a href="javascript:void(0)"><i class=" fa-fw">G</i><span class="hide-menu">Third Level Item</span></a> </li>
+                                <li> <a href="javascript:void(0)"><i class=" fa-fw">T</i><span class="hide-menu">Normal</span></a> </li>
+                                <li> <a href="javascript:void(0)"><i class=" fa-fw">M</i><span class="hide-menu">Important</span></a> </li>
+                                <li> <a href="javascript:void(0)"><i class=" fa-fw">R</i><span class="hide-menu">Urgent</span></a> </li>
                             </ul>
+                        </li>
+                        <li> <a href="javascript:void(0)"><i data-icon="7" class="glyphicon glyphicon-check"></i><span class="hide-menu">Completed Task</span></a> </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#"><i class="glyphicon glyphicon-phone-alt"></i> Telephone Directory<span class="fa arrow"></span></a>
+
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="#"><i class="glyphicon glyphicon-pencil"></i> Create New Telephone</a>
+                        </li>
+                        
+                        <li>
+                            <a href="javascript:;"><i class="	glyphicon glyphicon-earphone"></i> Show Telephone List</a>
                         </li>
                     </ul>
                 </li>
